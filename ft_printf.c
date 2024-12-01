ba2a5d31522712a7e18ed11e9667991dc92414f0
Yassine <yassineparis26@gmain.com>
2024-12-01 16:08:07 +0100

    jf

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: yassine <yassine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 16:56:12 by yassine           #+#    #+#             */
-/*   Updated: 2024/12/01 14:19:12 by yassine          ###   ########.fr       */
+/*   Updated: 2024/12/01 16:02:11 by yassine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,18 @@ int formaspecifier(char c, va_list track)
 {
 	int count;
 	count = 0;
-	// if (c == 'i' || c == 'd')
-	// 	count += ft_putnbr(va_arg(track, int));
-	// if (c == 'c')
-	// 	count += ft_putchar(va_arg(track, int));
-	if (c == 's')
+	if (c == 'i' || c == 'd')
+		count += ft_putnbr(va_arg(track, int));
+	else if (c == 'c')
+		count += ft_putchar(va_arg(track, int));
+	else if (c == 's')
 		count += ft_putstr(va_arg(track, char *));
 	// else if (c == 'x' || c == 'X')
-	// 	count += ft_hexaput(va_arg(track, int));
+	// 	count += ft_hexaput(va_arg(track, int), c);
 	// else if (c == 'p')
 	// 	count += ft_putnbr(va_arg(track, int));
-	// else if (c == '%')
-	// 	count += ft_putchar('%);
+	else if (c == '%')
+		count += ft_putchar('%');
 	return (count);
 }
 
--- ft_printf.h
+++ ft_printf.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ykabili- <ykabili-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yassine <yassine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 18:10:47 by yassine           #+#    #+#             */
-/*   Updated: 2024/12/01 12:31:19 by ykabili-         ###   ########.fr       */
+/*   Updated: 2024/12/01 16:01:42 by yassine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define FT_PRINTF_H
 # include <unistd.h>
 # include <stdarg.h>
+# include <stdio.h>
 
 int	ft_printf(const char *form, ...);
 int formaspecifier(char c, va_list track);
